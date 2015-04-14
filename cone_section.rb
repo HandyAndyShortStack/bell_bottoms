1d73ddf7a5e4dbaf6827b1c4fe55d43fff4cfacf
Andy Short <andynshort@gmail.com>
2015-04-14 15:20:35 -0700

    solve the big sides

--- cone_section.rb
+++ cone_section.rb
@@ -1,5 +1,3 @@
-require "./triangle"
-
 class ConeSection
 
   def initialize hsh
@@ -9,10 +7,28 @@ class ConeSection
   end
 
   def solve
-    little_triangle = Triangle.new({
-      a: (@base_diameter - @top_diameter) / 2,
-      C: 90.to_f,
-      b: @height
-    }).solve
+    big_triangle_sides
+  end
+
+private
+  
+  def big_triangle_sides
+
+    # solve the little sides
+    little_a = @height
+    little_b = @base_diameter - @top_diameter / 2
+    little_c = Math.sqrt((little_a ** 2) + (little_b ** 2))
+
+    # solve the big sides
+    big_b = @base_diameter / 2
+    coefficient = big_b / little_b
+    big_a = little_a * coefficient
+    big_c = little_c * coefficient
+
+    {a: big_a, b: big_b, c: big_c}
+  end
+
+  def radians degrees
+    degrees * Math::PI / 180
   end
 end
