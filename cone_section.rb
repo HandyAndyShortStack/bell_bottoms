45bc888140d2bd30fc1a59718ba8ad6c42d240ae
Andy Short <andynshort@gmail.com>
2015-04-14 15:40:05 -0700

    refactor a bit

--- cone_section.rb
+++ cone_section.rb
@@ -1,9 +1,10 @@
 class ConeSection
-
   def initialize hsh
     @height = hsh[:height].to_f
     @base_diameter = hsh[:base_diameter].to_f
     @top_diameter = hsh[:top_diameter].to_f
+    @little_triangle_sides = little_triangle_sides
+    @big_triangle_sides = big_triangle_sides
   end
 
   def solve
@@ -11,21 +12,27 @@ class ConeSection
   end
 
 private
+
+  def angles
+  end
+
+  def little_triangle_sides
+    little_b = (@base_diameter - @top_diameter) / 2
+    {
+      a: @height,
+      b: little_b,
+      c: Math.sqrt((@height ** 2) + (little_b ** 2))
+    }
+  end
   
   def big_triangle_sides
-
-    # solve the little sides
-    little_a = @height
-    little_b = (@base_diameter - @top_diameter) / 2
-    little_c = Math.sqrt((little_a ** 2) + (little_b ** 2))
-
-    # solve the big sides
     big_b = @base_diameter / 2
-    coefficient = big_b / little_b
-    big_a = little_a * coefficient
-    big_c = little_c * coefficient
-
-    {a: big_a, b: big_b, c: big_c}
+    coefficient = big_b / @little_triangle_sides[:b]
+    {
+      a: @little_triangle_sides[:a] * coefficient,
+      b: @little_triangle_sides[:b] * coefficient,
+      c: @little_triangle_sides[:c] * coefficient
+    }
   end
 
   def radians degrees
