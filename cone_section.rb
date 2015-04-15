1a18836954ed5004f24a5805dbe7d359deb3694e
Andy Short <andynshort@gmail.com>
2015-04-14 18:22:00 -0700

    implement solution

--- cone_section.rb
+++ cone_section.rb
@@ -5,26 +5,23 @@ class ConeSection
     @top_diameter = hsh[:top_diameter].to_f
     @little_triangle_sides = little_triangle_sides
     @big_triangle_sides = big_triangle_sides
-    @angles = angles
+    # @angles = angles
   end
 
   def solve
-    angles
+    # see http://mathcentral.uregina.ca/QQ/database/QQ.09.99/john2.html
+    c = 2 * Math::PI * (@base_diameter / 2)
+    t = (180 * c) / (Math::PI * @big_triangle_sides[:c])
+    d = @big_triangle_sides[:c] - @little_triangle_sides[:c]
+
+    puts "Begin by drawing a circle with radius #{@big_triangle_sides[:c]}."
+    puts "Next, draw a smaller circle with radius #{d} and the same center point."
+    puts "Mark out a sector of the larger circle with an angle of #{t} degrees."
+    puts "The band between the perimeter of the larger circle and the perimeter"
+    puts "of the smaller circle within this sector is the shape you will use for your pants."
   end
 
 private
-
-  def angles
-    a = @big_triangle_sides[:a]
-    b = @big_triangle_sides[:b]
-    c = @big_triangle_sides[:c]
-    angle_a = Math.acos (((b ** 2) + (c ** 2) - (a ** 2)) / (2 * b * c)) 
-    {
-      a: angle_a.to_degrees,
-      b: 90.0 - angle_a.to_degrees,
-      c: 90.0
-    }
-  end
 
   def little_triangle_sides
     little_b = (@base_diameter - @top_diameter) / 2
@@ -44,15 +41,27 @@ private
       c: @little_triangle_sides[:c] * coefficient
     }
   end
+
+  # def angles
+  #   a = @big_triangle_sides[:a]
+  #   b = @big_triangle_sides[:b]
+  #   c = @big_triangle_sides[:c]
+  #   angle_a = Math.acos (((b ** 2) + (c ** 2) - (a ** 2)) / (2 * b * c)) 
+  #   {
+  #     a: angle_a.to_degrees,
+  #     b: 90.0 - angle_a.to_degrees,
+  #     c: 90.0
+  #   }
+  # end
 end
 
-class Numeric
+# class Numeric
 
-  def to_degrees
-    self * (1 / (Math::PI / 180))
-  end
+#   def to_degrees
+#     self * (1 / (Math::PI / 180))
+#   end
 
-  def to_radians
-    self * (Math::PI / 180)
-  end
-end
+#   def to_radians
+#     self * (Math::PI / 180)
+#   end
+# end
